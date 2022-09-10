30ad63c20c832ad7ee78f195600c8c701d752535
shivenducs1136 <77795429+shivenducs1136@users.noreply.github.com>
2022-09-11 02:33:02 +0530

    Alarm when shake added

--- app/src/main/java/com/bitwisor/sekura/fragments/HomeFragment.kt
+++ app/src/main/java/com/bitwisor/sekura/fragments/HomeFragment.kt
@@ -6,7 +6,6 @@ import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,6 +13,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat.getSystemService
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
 import androidx.work.WorkRequest
@@ -26,7 +26,7 @@ import kotlin.math.sqrt
 
 class HomeFragment : Fragment() {
 
-    lateinit var binding:FragmentHomeBinding
+    lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
