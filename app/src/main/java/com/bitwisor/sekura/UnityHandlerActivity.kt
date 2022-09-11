80bad75946e604012a209d31b2c39ddb4c9b31fe
shivenducs1136 <77795429+shivenducs1136@users.noreply.github.com>
2022-09-11 19:14:28 +0530

    final

---
+++ app/src/main/java/com/bitwisor/sekura/UnityHandlerActivity.kt
@@ -0,0 +1,14 @@
+package com.bitwisor.sekura
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UnityHandlerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_unity_handler)
+//        val i = Intent(this,UnityPlayerActivity::class.java)
+//        startActivity(i)
+    }
+}
--- app/src/main/java/com/bitwisor/sekura/fragments/HomeFragment.kt
+++ app/src/main/java/com/bitwisor/sekura/fragments/HomeFragment.kt
@@ -1,16 +1,22 @@
 package com.bitwisor.sekura.fragments
 
 import android.content.Intent
-import android.os.Build
+import android.net.Uri
 import android.os.Bundle
 import android.view.*
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import androidx.core.content.ContextCompat.startActivity
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
 import androidx.work.WorkRequest
 import com.bitwisor.sekura.EmergencyActivity
+import com.bitwisor.sekura.MainActivity
 import com.bitwisor.sekura.R
+import com.bitwisor.sekura.UnityHandlerActivity
 import com.bitwisor.sekura.databinding.FragmentHomeBinding
 import com.bitwisor.sekura.uitls.ShakeWorker
 import com.bumptech.glide.Glide
@@ -22,6 +28,7 @@ class HomeFragment : Fragment() {
 
     lateinit var binding:FragmentHomeBinding
     lateinit var auth:FirebaseAuth
+    var num ="112"
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
@@ -53,11 +60,49 @@ class HomeFragment : Fragment() {
         binding.locateBtn.setOnClickListener {
             findNavController().navigate(R.id.action_homeFragment_to_fragment_maps)
         }
-
-        binding.helplineWebView.setOnClickListener {
-            findNavController().navigate(R.id.action_homeFragment_to_webView_fragment)
+        val languages = resources.getStringArray(R.array.helpline_nos)
+        val arrayAdapter = ArrayAdapter(requireContext(),R.layout.dropdown_layout, languages)
+        val autocompleteTV = view.findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView)
+        autocompleteTV.setAdapter(arrayAdapter)
+        autocompleteTV.addTextChangedListener {
+            when(it.toString()) {
+                "POLICE" -> num = "100"
+                "FIRE" -> num = "101"
+                "AMBULANCE" -> num = "102"
+                "Women Helpline" -> num = "1091"
+                "Women Helpline - ( Domestic Abuse )" -> num = "181"
+                "Anti Poison ( New Delhi )" -> num = "1066"
+                "Senior Citizen Helpline" -> num = "14567"
+                "Tourist Helpline" -> num = "1363"
+                "CYBER CRIME HELPLINE" -> num = "155620"
+                else -> num = "112"
+            }
         }
+        binding.callgreen.setOnClickListener {
 
+            callNum(num)
+        }
+        binding.bluecard.setOnClickListener {
+            var i = Intent(requireActivity(),UnityHandlerActivity::class.java)
+            startActivity(i)
+
+        }
+        binding.c1.setOnClickListener {
+            callNum("8171301100")
+        }
+        binding.c2.setOnClickListener {
+            callNum("9058905071")
+        }
+        binding.c3.setOnClickListener {
+            callNum("8707353287")
+        }
+        binding.c4.setOnClickListener {
+            callNum("9643294482")
+        }
     }
-
+    fun callNum(num:String){
+        val callIntent = Intent(Intent.ACTION_CALL)
+        callIntent.data = Uri.parse("tel:${num}")
+        startActivity(callIntent)
+    }
 }
